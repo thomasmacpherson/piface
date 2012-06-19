1deefa2e919abbc9e1eabf24d62b3bc1008a8522
Thomas Preston <thomasmarkpreston@gmail.com>
2012-06-19 10:54:32 +0100

    added a pfio stub, it needs replacing with the actual pfio.py file (or whatever the IO module is going to be called

---
+++ twitter_chicken/pfio.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+THIS IS A STUB AND SHOULD BE REPLACED BY THE ACTUAL PFIO MODULE!!!
+
+If you write the pfio module in another directory then just link to it
+in this one using:
+
+	$ ln -s ../relative/path/to/actual/pfio.py pfio.py
+
+Don't bother adding the link to the git repo since it may not stay
+relative and we can always just recreate it.
+"""
+
+def digitalWrite(port, value):
+	print "PFIO=> writing %i to port %i" % (value, port)
