0a2ce316b49eeee223d21f7b9198ee3ad494e783
Tom Macpherson-Pope <thomasmacpherson@live.co.uk>
2012-06-20 16:57:09 +0100

    started writing twitterModeCube.py

---
+++ build/lib.linux-armv6l-2.6/twitterMoodCube.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+twitterMoodCube.py
+
+Simple moode cube that reflects the mood of the world and allows you to send your mood via a status post
+author: Thomas Macpherson-Pope
+date  : 20/06/2012
+"""
+
+from time import sleep
+import twitter
+import pfio
+
+
+pfio.init()
+
+twitter = twitter.Api()
+
+terms = ["#happy","#sad","#angry","#jelous","#guilty"]
+
+search_term = terms[0]
+
+twitter.GetSearch(term=search_term)
