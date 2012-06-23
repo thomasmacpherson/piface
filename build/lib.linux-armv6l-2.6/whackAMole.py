43907b0fd0e3f09f844ddf844850e9bdb8695791
thomasmacpherson <thomasmacpherson@live.co.uk>
2012-06-24 00:12:47 +0100

    finished whackAMole.py

--- build/lib.linux-armv6l-2.6/whackAMole.py
+++ build/lib.linux-armv6l-2.6/whackAMole.py
@@ -20,72 +20,84 @@ def next_colour():
 
 
 
-current = next_colour() 	# create first random colour to be lit
-pfio.digital_write(current+2,1)	# turn colour on
-set_time = 2000		# time allowed to hit each light (starts off large and reduced after each hit)
-time_left = set_time		# countdown timer for hitting the light
-hit = 0				# the input value
+current = next_colour() 			# create first random colour to be lit
+pfio.digital_write(current+2,1)			# turn colour on
+set_time = 2000					# time allowed to hit each light (starts off large and reduced after each hit)
+time_left = set_time				# countdown timer for hitting the light
+hit = 0						# the input value
 score = 0
 misses = 0
 
-colours = ["Red","Green","Blue","Yellow","White"]
-
+colours = ["Red","Green","Blue","Yellow","White"]	# colour list for printing to screen
+previous_pressed = 255
 
 
 print "Time left is: %s" %time_left
 
+
 while True:
-	#print pfio.read_input
+
 	in_bit_pattern = pfio.read_input()[2] ^ 0b11111111 # see if any buttons have been hit
-	#TODO: insert check for button release (while 0)
-	if in_bit_pattern > 0:
-		print "in bit pattern:      %s" % bin(in_bit_pattern)
-		print bin(current)
-		print "current bit pattern: %s" % bin(pfio.get_pin_bit_mask(current))
-		if in_bit_pattern == pfio.get_pin_bit_mask(current):	# check that only the correct button was hit
-			pfio.digital_write(current+2, 0)	# turn off hit light
-			previous = current
-			current = next_colour()		# get next colour
-			while current == previous:		# ensure differnt colour each time
-				current = next_colour()		# get next colour
-				
-			if ((score + misses) %50) ==49:
-					set_time /= 2			# reduce the allowed time
-					print "Time left is: %s" %time_left
-			time_left = set_time		# set the countdown time
+	
+	if in_bit_pattern != previous_pressed:		# check this is a new button press
+		previous_pressed = in_bit_pattern	# record button press for next time's check
+
+		if in_bit_pattern > 0:
+
+			if in_bit_pattern == pfio.get_pin_bit_mask(current):	# check that only the correct button was hit
 			
-			score += 1
-			pfio.digital_write(current+2,1)	# turn the new light on
+				pfio.digital_write(current+2, 0)		# turn off hit light
+				previous = current
+				current = next_colour()				# get next colour
+			
+				while current == previous:			# ensure differnt colour each time
+					current = next_colour()			# get next colour
+				
+				if ((score + misses) %30) ==49:
+					if set_time > 100:
+						set_time /= 2			# reduce the time allowed to hit the light
+						print "Time left is: %s" %set_time
+			
+				time_left = set_time				# set the countdown time
+			
+				score += 1
+				print "Your score %d" %score
+				pfio.digital_write(current+2,1)			# turn the new light on
+			
 
-			#sleep(1)			# leave the light on for a second
-
-		else:
-			print "shit"
-			score -= 1
+			else:							# wrong button pressed
+				print "Wrong one!"
+				print "Your score %d" %score
+				score -= 1
+			
+			
 	elif time_left==0:
-			pfio.digital_write(current+2, 0)	# turn off hit light
-			misses +=1
-			if misses == 10:
-				break
-			previous = current
-			current = next_colour()			# get next colour
-			while current == previous:		# ensure differnt colour each time
-				current = next_colour()		# get next colour
-				
-			if ((score + misses) %50)==49:
-					set_time /= 2		# reduce the allowed time
-					print "Time left is: %s" %time_left
-			time_left = set_time		# set the countdown time
+		pfio.digital_write(current+2, 0)			# turn off hit light
+		misses +=1						# increment misses
+		print "Missed one!"
+		if misses == 10:					# too many misses = Game Over!
+			break
 			
-			score += 1
-			pfio.digital_write(current+2,1)	# turn the new light on		
+		previous = current					#
+		current = next_colour()					# get next colour
+		while current == previous:				# ensure differnt colour each time
+			current = next_colour()				# get next colour
+				
+		if ((score + misses) %30)==49:
+			if set_time > 100:
+				set_time /= 2				# reduce the allowed time
+				print "Time left is: %s" %set_time
+		time_left = set_time					# set the countdown time
+			
+		pfio.digital_write(current+2,1)				# turn the new light on		
 	
-	time_left -=1	# decrement the time left to hit the current light
+	time_left -=1							# decrement the time left to hit the current light
 
 	
 
-pfio.write_output(0)	# turn all lights off	
-print "Your score was: %s" %score
-#pfio.deinit()		# close the pfio
+pfio.write_output(0)				# turn all lights off	
+print "\nGame over!\n"
+print "Your score was: %s" %score		# print the player's final score
+#pfio.deinit()					# close the pfio
 	
 
