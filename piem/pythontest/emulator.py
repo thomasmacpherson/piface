a961164a12eae0ac7d3b097cea71faeb3d82b8b8
Thomas Preston <thomasmarkpreston@gmail.com>
2012-06-26 12:14:05 +0100

    emulator now at v1.0, it works - needs testing (what doesn't?)

--- piem/pythontest/emulator.py
+++ piem/pythontest/emulator.py
@@ -18,20 +18,31 @@
 
 import pygtk
 import gtk, gobject, cairo
+import threading
 from gtk import gdk
 from math import pi
+import time
 
-status = 0
+VERBOSE_MODE = False
+
+EMU_WIDTH  = 292
+EMU_HEIGHT = 193
+EMU_SPEED  = 20
+EMU_PRINT_PREFIX = "EMU:"
+
+PIN_COLOUR_R = 0
+PIN_COLOUR_G = 1
+PIN_COLOUR_B = 1
 
 # pin circle locations
 ledsX = [183.0,183.0,222.0,239.0]
 ledsY = [135.0,78.0,27.0,27.0]
 switchesX = [15.0, 40.0, 65.0, 90.0]
 switchesY = [160.0, 160.0, 160.0, 160.0]
-relay1PinsX = [285.0,285.0,285.0,]
-relay1PinsY = [73.0,86.0,98.0]
-relay2PinsX = [285.0,285.0,285.0]
-relay2PinsY = [124.0,136.0,148.0]
+relay1PinsX = [285.0,285.0,285.0]
+relay1PinsY = [124.0,136.0,148.0]
+relay2PinsX = [285.0,285.0,285.0,]
+relay2PinsY = [73.0,86.0,98.0]
 boardInputPinsX = [6.0,19.0,31.0,44.0,56.0,68.0,80.0,92.0,104]
 boardInputPinsY = [186.0,186.0,186.0,186.0,186.0,186.0,186.0,186.0,186.0]
 boardOutputPinsX = [181.0,194.0,206.0,218.0,230.0,242.0,254.0,266.0,278.0]
@@ -39,13 +50,6 @@ boardOutputPinsY = [8.0,8.0,8.0,8.0,8.0,8.0,8.0,8.0,8.0]
 
 RELAY_PIN_PATTERN_ON  = (0, 1, 1)
 RELAY_PIN_PATTERN_OFF = (1, 1, 0)
-
-HIGH = 1
-LOW = 0
-INPUT = 1
-OUTPUT = 0
-pinModes = 0
-
 
 # piface peripheral pin numbers
 # each peripheral is connected to an I/O pin
@@ -70,14 +74,30 @@ class Item(object):
 		# an item defaults to an output device
 		self.pin_number = pin_number
 		self.is_input = is_input
-		self.value = 0
 
-	def turn_on(self):
+		self._value = 0 # hidden value for property stuff
+		self._hold  = False # this value cannot be changed unless forced
+		self._force = False # when true, held values can be changed
+	
+	def _get_value(self):
+		return self._value
+
+	def _set_value(self, new_value):
+		if not self._hold or (self._hold and self._force):
+			self._value = new_value
+			self._hold  = False
+			self._force = False
+	
+	value = property(_get_value, _set_value)
+
+	def turn_on(self, hold=False):
 		#print "turning on"
 		self.value = 1;
-	
-	def turn_off(self):
+		self._hold = hold
+
+	def turn_off(self, force=False):
 		#print "turning off..."
+		self._force = force
 		self.value = 0;
 
 class Pin(Item):
@@ -105,27 +125,36 @@ class Pin(Item):
 		if self.value == 1:
 			#print "drawing pin at %d, %d" % (self.x, self.y)
 			cr.save()
-			cr.set_source_rgb(1,1,0)
+			cr.set_source_rgb(PIN_COLOUR_R,PIN_COLOUR_G,PIN_COLOUR_B)
 			cr.arc (self.x, self.y, 5, 0, 2*pi);
 			cr.fill()
 			cr.restore()
 
 class LED(Item):
 	"""A virtual LED on the RaspberryPi emulator"""
-	def __init__(self, led_number):
+	def __init__(self, led_number, attached_pin):
+		self.attached_pin = attached_pin
+
 		self.x = ledsX[led_number-1]
 		self.y = ledsY[led_number-1]
 
-		if led_number == 1:
-			pin_number = PH_PIN_LED_1
-		elif led_number == 2:
-			pin_number = PH_PIN_LED_2
-		elif led_number == 3:
-			pin_number = PH_PIN_LED_3
-		else:
-			pin_number = PH_PIN_LED_4
+		Item.__init__(self, led_number)
 
-		Item.__init__(self, pin_number)
+	def _get_value(self):
+		return self.attached_pin.value
+
+	def _set_value(self, new_value):
+		self.attached_pin.value = new_value
+	
+	value = property(_get_value, _set_value)
+
+	def turn_on(self):
+		#print "turning on"
+		self.value = 1;
+	
+	def turn_off(self):
+		#print "turning off..."
+		self.value = 0;
 
 	def draw(self, cr):
 		"""
@@ -149,27 +178,28 @@ class LED(Item):
 
 class Relay(Item):
 	"""A relay on the RaspberryPi"""
-	def __init__(self, relay_number):
+	def __init__(self, relay_number, attached_pin):
+		self.attached_pin = attached_pin
 		if relay_number == 1:
-			pin_number = PH_PIN_RELAY_1
 			self.pins = [Pin(i, False, True, False) for i in range(3)]
 		else:
-			pin_number = PH_PIN_RELAY_2
 			self.pins = [Pin(i, False, False, True) for i in range(3)]
 
-		self._value = 0 # hidden value for property stuff
-		self.pins[0].value, \
-			self.pins[1].value, \
-			self.pins[2].value = RELAY_PIN_PATTERN_OFF
+		Item.__init__(self, relay_number)
 
-		Item.__init__(self, pin_number)
-	
+		self.value = self.attached_pin.value
+
 	def _get_value(self):
-		return self._value
+		return self.attached_pin.value
 
 	def _set_value(self, new_value):
-		# set the pins
-		if new_value >= 1:
+		self.attached_pin.value = new_value
+		self.set_pins()
+
+	value = property(_get_value, _set_value)
+
+	def set_pins(self):
+		if self.value >= 1:
 			self.pins[0].value, \
 				self.pins[1].value, \
 				self.pins[2].value = RELAY_PIN_PATTERN_ON
@@ -178,29 +208,41 @@ class Relay(Item):
 				self.pins[1].value, \
 				self.pins[2].value = RELAY_PIN_PATTERN_OFF
 
-		self._value = new_value
+	def turn_on(self):
+		self.value = 1;
+	
+	def turn_off(self):
+		self.value = 0;
 
 	def draw(self, cr):
+		self.set_pins()
 		for pin in self.pins:
 			#print "Drawing from relay %d" % self.pin_number
 			pin.draw(cr)
 
 class Switch(Item):
 	"""A virtual switch on the RaspberryPi emulator"""
-	def __init__(self, switch_number):
+	def __init__(self, switch_number, attached_pin):
+		self.attached_pin = attached_pin
 		self.x = switchesX[switch_number-1]
 		self.y = switchesY[switch_number-1]
-
-		if switch_number == 1:
-			switch_number = PH_PIN_SWITCH_1
-		elif switch_number == 2:
-			switch_number = PH_PIN_SWITCH_2
-		elif switch_number == 3:
-			switch_number = PH_PIN_SWITCH_3
-		else:
-			switch_number = PH_PIN_SWITCH_4
-
 		Item.__init__(self, switch_number, True)
+
+	def _get_value(self):
+		return self.attached_pin.value
+
+	def _set_value(self, new_value):
+		self.attached_pin.value = new_value
+	
+	value = property(_get_value, _set_value)
+
+	def turn_on(self):
+		#print "turning on"
+		self.value = 1;
+	
+	def turn_off(self):
+		#print "turning off..."
+		self.value = 0;
 
 	def draw_hidden(self, cr):
 		cr.arc(self.x, self.y, 5, 0, 2*pi)
@@ -232,27 +274,40 @@ class Screen(gtk.DrawingArea):
 		self.set_size_request(w, h)
 		self.x, self.y = 11110,11111110 # unlikely first coord to prevent false hits.
 
+		self.button_pressed = False # check if it was a mouse move or button
+
 	## When expose event fires, this is run
-	def do_expose_event( self, widget, event ):
+	def do_expose_event(self, widget, event):
 		self.cr = self.window.cairo_create( )
 		## Call our draw function to do stuff.
 		self.draw()
 
-	def _mouseMoved ( self, widget, event ):
+	def _mouseMoved(self, widget, event):
 		self.x = event.x
 		self.y = event.y
+		self.button_pressed = False
 		self.queue_draw_area(0, 0, 350, 350)
 
+	def _button_press(self, widget, event):
+		self.x = event.x
+		self.y = event.y
+		self.button_pressed = True
+		self.queue_draw_area(0, 0, 350, 350)
+	
+	def qdraw(self):
+		"""Register a draw to be made"""
+		self.queue_draw_area(0, 0, 350, 350)
 
 class EmulatorScreen(Screen):
 	"""This class is also a Drawing Area, coming from Screen."""
 	def __init__ (self, w, h, speed):
 		Screen.__init__(self, w, h, speed)
-		self.switches = [Switch(i) for i in range(1,5)] # make four switches
-		self.input_pins = [Pin(i, True) for i in range(1,10)] # make nine input Pins
-		self.leds = [LED(i) for i in range(1,5)] # make four LED's
-		self.relays = [Relay(i) for i in range(1,3)] # make two relays
-		self.output_pins = [Pin(i) for i in range(1,10)] # make nine output pins
+		self.input_pins = [Pin(i, True) for i in range(1,9)]
+		self.switches = [Switch(i+1, self.input_pins[i]) for i in range(4)]
+
+		self.output_pins = [Pin(i) for i in range(1,10)]
+		self.relays = [Relay(i+1, self.output_pins[i]) for i in range(2)]
+		self.leds = [LED(i+1, self.output_pins[i]) for i in range(4)]
 
 	def draw(self):
 		cr = self.cr # Shabby shortcut.
@@ -272,23 +327,11 @@ class EmulatorScreen(Screen):
 		cr.new_path() # stops the hit shape from being drawn
 		cr.restore()
 
-		# detect clicks on the switches (TODO: this should be roll over)
-		for switch in self.switches:
-			switch.draw_hidden(cr) 
-			if self.mouse_hit(cr):
-				if switch.value == 1:
-					switch.turn_off()
-				else:
-					switch.turn_on()
-
-		# detect clicks on the input input_pins
-		for pin in self.input_pins:
-			pin.draw_hidden(cr) 
-			if self.mouse_hit(cr):
-				if pin.value == 1:
-					pin.turn_off()
-				else:
-					pin.turn_on()
+		if self.button_pressed:
+			self.input_pin_detect(cr)
+			self.button_pressed = False # stop registering the press
+		else:
+			self.switch_detect(cr)
 
 		# draw all the switches
 		for switch in self.switches:
@@ -310,6 +353,25 @@ class EmulatorScreen(Screen):
 		for pin in self.output_pins:
 			pin.draw(cr)
 
+	def switch_detect(self, cr):
+		# detect rollover on the switches
+		for switch in self.switches:
+			switch.draw_hidden(cr) 
+			if self.mouse_hit(cr):
+				switch.turn_on()
+			else:
+				switch.turn_off()
+
+	def input_pin_detect(self, cr):
+		# detect clicks on the input input_pins
+		for pin in self.input_pins:
+			pin.draw_hidden(cr) 
+			if self.mouse_hit(cr):
+				if pin.value == 1:
+					pin.turn_off(True) # force/hold
+				else:
+					pin.turn_on(True) # force/hold
+
 	def mouse_hit(self, cr):
 		cr.save ( ) # Start a bubble
 		cr.identity_matrix ( ) # Reset the matrix within it.
@@ -317,16 +379,6 @@ class EmulatorScreen(Screen):
 		cr.new_path ( ) # stops the hit shape from being drawn
 		cr.restore ( ) # Close the bubble like this never happened.
 		return hit
-
-	def _button_press( self, widget, event ):
-		self.x = event.x
-		self.y = event.y
-		self.queue_draw_area(0, 0, 350, 350)
-
-	def _mouseMoved(self, widget, event):
-		#self.queue_draw_area(0, 0, 350, 350)
-		#print "The mouse is at (%d, %d)" % (event.x, event.y)
-		pass
 
 """
 ## Print to the console -- low-tech special effects :)
@@ -361,56 +413,140 @@ class EmulatorScreen(Screen):
 		self.cr.arc(boardButtonsX[index],boardButtonsY[index],6,0, 2*pi)
 """
 
+#emu_screen = None
+class Emulator(threading.Thread):
+	def __init__(self):
+		gtk.gdk.threads_init() # init the gdk threads
+		threading.Thread.__init__(self)
 
-def get_pin_bit_mask(pin):
-	return 1 << (pin-1)
+	def run(self):
+		window = gtk.Window()
+		window.connect("delete-event", gtk.main_quit)
+		global emu_screen
+		emu_screen = EmulatorScreen(EMU_WIDTH, EMU_HEIGHT, EMU_SPEED)
+		emu_screen.show()
+		window.add(emu_screen) # add the screen to the window
+		window.present()
+		gtk.main()
 
-def digital_read(pin):
-	checkPinMode(pin, INPUT)
-	global inputPins
-	print 'Read of pin {}'.format(pin)
-	pin = pinTranslation(pin)
-	pin = pin & inputPins
-	if pin:
-		pin = 1
-	print pin
-	return pin
+def init():
+	"""Initialises the RaspberryPi emulator"""
+	rpi_emulator = Emulator()
+	rpi_emulator.start()
+	time.sleep(0.5)
+	
+	"""
+	EXAMPLE
+	global emu_screen
+	emu_screen.output_pins[2].turn_on()
+	emu_screen.qdraw()
 
+	time.sleep(3)
+	emu_screen.output_pins[5].turn_on()
+	emu_screen.qdraw()
+	"""
 
-def digital_write(pin,status):
-	global outputPins
-	pin = pinTranslation(pin)
-	if status:
-		outputPins = outputPins | pin
+def deinit():
+	"""Deinitialises the PiFace"""
+	gtk.main_quit()
+	global emu_screen
+	emu_screen = None
+
+def emu_print(text):
+	"""Prints a string with the pfio print prefix"""
+	print "%s %s" % (EMU_PRINT_PREFIX, text)
+
+def get_pin_bit_mask(pin_number):
+	"""Translates a pin number to pin bit mask. First pin is pin1 (not pin0).
+	pin3 = 0b00000100
+	pin4 = 0b00001000
+
+	TODO: throw and exception if the pin number is out of range
+	"""
+	#return 2**(pin_number-1)
+	return 1 << (pin_number - 1) # shifting makes more sense
+
+def get_pin_number(bit_pattern):
+	"""Returns the lowest pin number from a given bit pattern"""
+	pin_number = 1 # assume pin 1
+	while (bit_pattern & 1) == 0:
+		bit_pattern = bit_pattern >> 1
+		pin_number += 1
+		if pin_number > 8:
+			pin_number = 0
+			break
+	
+	return pin_number
+
+def build_hex_string(items):
+	"""Builds a hexidecimal string comprised of the given items"""
+	hex_string = ""
+	for item in items:
+		hex_string += "%02x" % item # 10 = 0a
+	return hex_string
+
+def digital_write(pin_number, value):
+	"""Writes the value given to the pin specified"""
+	if VERBOSE_MODE:
+		emu_print("digital write start")
+
+	global emu_screen
+	if value >= 1:
+		emu_screen.output_pins[pin_number-1].turn_on()
 	else:
-		pin = ~pin
-		outputPins = outputPins & pin
+		emu_screen.output_pins[pin_number-1].turn_off()
+	
+	emu_screen.qdraw()
 
-		print 'Output pins {}'.format(outputPins)
+	if VERBOSE_MODE:
+		emu_print("digital write end")
+
+def digital_read(pin_number):
+	"""Returns the value of the pin specified"""
+	global emu_screen
+	return emu_screen.input_pins[pin_number-1].value
 
 """
-def physically_change_pin(pin):
-	global inputPins
-	pin +=1
-	pin = pinTranslation(pin)
-
-	if status:
-		inputPins = inputPins | pin
-	else:
-		inputPins = intputPins & ~pin
-
-	print "Input pins"
-	print inputPins
+Some wrapper functions so the user doesn't have to deal with
+ugly port variables
 """
+def read_output():
+	"""Returns the values of the output pins"""
+	global emu_screen
+	output_pin_values = [pin.value for pin in emu_screen.output_pins]
+	output_binary_string = "".join(map(str, output_pin_values))
+	return int(output_binary_string, 2)
 
-def run(Widget, w, h, speed):
-	window = gtk.Window()
-	window.connect("delete-event", gtk.main_quit)
-	widget = Widget(w, h, speed)
-	widget.show()
-	window.add(widget)
-	window.present()
-	gtk.main()
+def read_input():
+	"""Returns the values of the input pins"""
+	global emu_screen
+	input_pin_values = [pin.value for pin in emu_screen.input_pins]
+	input_binary_string = "".join(map(str, input_pin_values))
+	return int(input_binary_string, 2)
+
+def write_output(data):
+	"""Writes the values of the output pins"""
+	output_binary_string = bin(data)[2:].zfill(8) # get an 8 char binary string
+	global emu_screen
+	for i in range(8):
+		if output_binary_string[i] >= 1:
+			emu_screen.output_pins[i].turn_on()
+		else:
+			emu_screen.output_pins[i].turn_off()
+
+	emu_screen.qdraw()
+
+def write_input(data):
+	"""Writes the values of the input pins"""
+	input_binary_string = bin(data)[2:].zfill(8) # get an 8 char binary string
+	global emu_screen
+	for i in range(8):
+		if input_binary_string[i] >= 1:
+			emu_screen.input_pins[i].turn_on()
+		else:
+			emu_screen.input_pins[i].turn_off()
+
+	emu_screen.qdraw()
 
 if __name__ == "__main__":
-	run(EmulatorScreen, 292, 193, speed=20)
+	init()
