f5b7f5939bb083377dacd00056faa20f982e9aea
Thomas Preston <thomasmarkpreston@gmail.com>
2012-07-11 11:06:22 +0100

    updated scratch handler to deal better with quiting and also broadcasts the input names on initialisation (so scratch knows them)

--- scratch/scratch_handler.py
+++ scratch/scratch_handler.py
@@ -18,6 +18,7 @@ PORT = 42001
 DEFAULT_HOST = '127.0.0.1'
 #HOST = askstring('Scratch Connector', 'IP:')
 BUFFER_SIZE = 100
+SOCKET_TIMEOUT = 1
 
 SCRATCH_SENSOR_NAME_INPUT = (
     'piface-input1',
@@ -44,33 +45,46 @@ class ScratchSender(threading.Thread):
     def __init__(self, socket):
         threading.Thread.__init__(self)
         self.scratch_socket = socket
+        self._stop = threading.Event()
+
+        # make scratch aware of the pins
+        for i in range(len(SCRATCH_SENSOR_NAME_INPUT)):
+            self.broadcast_pin_update(i, 0)
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
 
     def run(self):
         last_bit_pattern = 0
-        while True:
+        while not self.stopped():
             pin_bit_pattern = pfio.read_input()
 
             # if there is a change in the input pins
             changed_pins = pin_bit_pattern ^ last_bit_pattern
             if changed_pins:
                 try:
-                    self.broadcast_pin_update(changed_pins, pin_bit_pattern)
+                    self.broadcast_changed_pins(changed_pins, pin_bit_pattern)
                 except Exception as e:
                     print e
                     break
 
             last_bit_pattern = pin_bit_pattern
 
-    def broadcast_pin_update(self, changed_pin_map, pin_value_map):
+    def broadcast_changed_pins(self, changed_pin_map, pin_value_map):
         for i in range(8):
             # if we care about this pin's value
             if (changed_pin_map >> i) & 0b1:
                 pin_value = (pin_value_map >> i) & 0b1
+                self.broadcast_pin_update(i, pin_value)
 
-                bcast_str = 'sensor-update "%s" %d' % (SCRATCH_SENSOR_NAME_INPUT[i], pin_value)
-
-                print 'sending: %s' % bcast_str
-                self.send_scratch_command(bcast_str)
+    def broadcast_pin_update(self, pin_index, value):
+        sensor_name = SCRATCH_SENSOR_NAME_INPUT[pin_index]
+        bcast_str = 'sensor-update "%s" %d' % (sensor_name, value)
+        print 'sending: %s' % bcast_str
+        self.send_scratch_command(bcast_str)
 
     def send_scratch_command(self, cmd):
         n = len(cmd)
@@ -85,16 +99,25 @@ class ScratchListener(threading.Thread):
     def __init__(self, socket):
         threading.Thread.__init__(self)
         self.scratch_socket = socket
+        self._stop = threading.Event()
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
 
     def run(self):
-        while True:
+        while not self.stopped():
             try:
                 data = self.scratch_socket.recv(BUFFER_SIZE)
                 length = struct.unpack(
                         '>i',
                         '%c%c%c%c' % (data[0], data[1], data[2], data[3])
                     )[0]
-            except: # TODO: specify and error here
+            except socket.timeout:
+                continue
+            except:
                 break
 
             #print 'Length: %d, Data: %s' % (length, data[4:])
@@ -125,6 +148,13 @@ def create_socket(host, port):
     scratch_sock.connect((host, port))
     return scratch_sock
 
+def cleanup_threads(threads):
+    for thread in threads:
+        thread.stop()
+
+    for thread in threads:
+        thread.join()
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         host = sys.argv[1]
@@ -133,12 +163,23 @@ if __name__ == '__main__':
 
     # open the socket
     print 'Connecting...' ,
-    socket = create_socket(host, PORT)
+    the_socket = create_socket(host, PORT)
     print 'Connected!'
+
+    the_socket.settimeout(SOCKET_TIMEOUT)
 
     pfio.init()
 
-    listener = ScratchListener(socket)
-    sender = ScratchSender(socket)
+    listener = ScratchListener(the_socket)
+    sender = ScratchSender(the_socket)
     listener.start()
     sender.start()
+
+    # wait for ctrl+c
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        cleanup_threads((listener, sender))
+        pfio.deinit()
+        sys.exit()
