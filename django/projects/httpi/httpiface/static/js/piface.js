890c6d1be8aadffa95025d1ae786ff1f02bfecd7
Thomas Preston <thomasmarkpreston@gmail.com>
2012-08-02 10:21:28 +0100

    finished the pi face control part of the web interface

--- django/projects/httpi/httpiface/static/js/piface.js
+++ django/projects/httpi/httpiface/static/js/piface.js
@@ -4,10 +4,20 @@ PIN_OUT_X = [266.0, 254.0, 242.0, 230.0, 218.0, 206.0, 194.0, 181.0];
 PIN_OUT_Y = [8.0,   8.0,   8.0,   8.0,   8.0,   8.0,   8.0,   8.0  ];
 PIN_R     = 5;
 
+SWITCH_X = [14.3,  39.3,  64.3,  89.3];
+SWITCH_Y = [157.5, 157.5, 157.5, 157.5];
+SWITCH_R = 5;
+
+RELAY_PINS_X = [[285.0, 285.0, 285.0], [285.0, 285.0, 285.0]]
+RELAY_PINS_Y = [[124.0, 136.0, 148.0], [ 73.0,  86.0,  98.0]]
+
+update_interval_id = 0;
+
 var error_handler = function(jqXHR, text_status, error_thrown)
 {
+    // stop auto updating
+    window.clearInterval(update_interval_id)
     alert(text_status + ": " + error_thrown + "\n" + jqXHR.responseText);
-    window.setInterval(0);
 };
 
 var send_ajax = function(datatosend, success_func)
@@ -26,7 +36,7 @@ var output_checkbox_clicked = function(the_event)
 {
     // generate binary
     var output_bitp = 0;
-    for (var i=8; i >= 1; i--) // first output is on rhs
+    for (var i=7; i >= 0; i--) // first output is on rhs
     {
         output_bitp = output_bitp << 1;
         if ($("#pifaceoutputcheckbox"+i).prop('checked'))
@@ -48,16 +58,90 @@ var draw_circle = function(context, x, y, r, colour)
     context.stroke();
 };
 
+var draw_in_pin = function(context, pin_number)
+{
+    draw_circle(context,
+            PIN_IN_X[pin_number],
+            PIN_IN_Y[pin_number],
+            PIN_R);
+};
+
+var draw_out_pin = function(context, pin_number)
+{
+    draw_circle(context,
+            PIN_OUT_X[pin_number],
+            PIN_OUT_Y[pin_number],
+            PIN_R);
+};
+
+var draw_switch = function(context, switch_number)
+{
+    draw_circle(context,
+            SWITCH_X[switch_number],
+            SWITCH_Y[switch_number],
+            SWITCH_R);
+};
+
+var draw_led_on = function(context, led_number)
+{
+    $("#pifaceled"+led_number).show();
+};
+
+var draw_led_off = function(context, led_number)
+{
+    $("#pifaceled"+led_number).hide();
+};
+
+var draw_relay_on = function(context, relay_number)
+{
+    draw_circle(context,
+            RELAY_PINS_X[relay_number][1],
+            RELAY_PINS_Y[relay_number][1],
+            PIN_R);
+    draw_circle(context,
+            RELAY_PINS_X[relay_number][2],
+            RELAY_PINS_Y[relay_number][2],
+            PIN_R);
+};
+
+var draw_relay_off = function(context, relay_number)
+{
+    draw_circle(context,
+            RELAY_PINS_X[relay_number][0],
+            RELAY_PINS_Y[relay_number][0],
+            PIN_R);
+    draw_circle(context,
+            RELAY_PINS_X[relay_number][1],
+            RELAY_PINS_Y[relay_number][1],
+            PIN_R);
+};
+
 var set_in_pin = function(context, pin_number, set_pin)
 {
     if (set_pin)
-        draw_circle(context, PIN_IN_X[pin_number], PIN_IN_Y[pin_number], PIN_R);
+    {
+        draw_in_pin(context, pin_number);
+        if (pin_number < 4)
+            draw_switch(context, pin_number);
+    }
 };
 
 var set_out_pin = function(context, pin_number, set_pin)
 {
     if (set_pin)
-        draw_circle(context, PIN_OUT_X[pin_number], PIN_OUT_Y[pin_number], PIN_R);
+    {
+        draw_out_pin(context, pin_number);
+        if (pin_number < 4)
+            draw_led_on(context, pin_number);
+            if (pin_number < 2)
+                draw_relay_on(context, pin_number);
+    }
+    else if (pin_number < 4)
+    {
+        draw_led_off(context, pin_number);
+        if (pin_number < 2) // relay is off
+            draw_relay_off(context, pin_number);
+    }
 };
 
 var update_piface = function(data, output_status, jqXHR)
@@ -86,12 +170,12 @@ var update_piface = function(data, output_status, jqXHR)
 var setup = function()
 {
     // set some event listeners
-    for (var i=1; i <= 8; i++)
+    for (var i = 0; i <= 7; i++)
         $("#pifaceoutputcheckbox"+i).click(output_checkbox_clicked);
 
     update_piface(); // update now
     // update the board every second
-    window.setInterval(update_piface, 1000);
+    update_interval_id = window.setInterval(update_piface, 1000);
 };
 
 $(document).ready(setup);
--- django/projects/httpi/httpiface/views.py
+++ django/projects/httpi/httpiface/views.py
@@ -5,6 +5,7 @@ import simplejson
 
 import piface.pfio as pfio
 
+"""
 # fake pfio stuff for testing
 outpins = 0
 inpins = 0
@@ -22,6 +23,7 @@ pfio.init = fakepfioinit
 pfio.write_output = fakepfiowrite
 pfio.read_input = fakepfioreadin
 pfio.read_output = fakepfioreadout
+"""
 
 
 def index(request):
@@ -35,7 +37,8 @@ def index(request):
         piface_error_msg = error
 
     return render_to_response("httpiface/index.html",
-            {'button_range' : range(1, 9),
+            {'button_range' : range(8),
+                'led_range' : range(4),
                 'piface_detected' : piface_detected,
                 'piface_error_msg' : piface_error_msg},
             context_instance=RequestContext(request))
@@ -50,21 +53,12 @@ def ajax(request):
         except ValueError:
             return HttpResponseBadRequest("write_output needs an integer bit pattern.")
 
-        __write_output(output_bitp)
+        pfio.write_output(output_bitp)
     
     if 'read_input' in data:
-        return_values.update({'input_bitp' : __read_input()})
+        return_values.update({'input_bitp' : pfio.read_input()})
 
     if 'read_output' in data:
-        return_values.update({'output_bitp' : __read_output()})
+        return_values.update({'output_bitp' : pfio.read_output()})
 
     return HttpResponse(simplejson.dumps(return_values))
-
-def __write_output(output_bitp):
-    pfio.write_output(output_bitp)
-
-def __read_input():
-    return pfio.read_input()
-
-def __read_output():
-    return pfio.read_output()
